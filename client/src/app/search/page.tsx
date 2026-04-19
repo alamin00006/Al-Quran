f6309c9348b774dc368ab2731bf12e473b2ac467
Mohammad Al Amin <alaminbamna08@gmail.com>
2026-04-19 19:36:50 +0600

    unused folder search removed

--- client/src/app/search/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Redirects legacy search route requests back to the home search experience.
-export default function SearchRedirect() {
-  redirect("/");
-}
